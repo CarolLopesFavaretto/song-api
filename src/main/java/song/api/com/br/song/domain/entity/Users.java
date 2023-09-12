a0ec4f2beebdbf362a8cda522e9fb2643b9752b2
Caroline <carolinelopesrdg@hotmail.com>
2023-09-11 21:45:25 -0300

    implementando entitys e config swagger

---
+++ src/main/java/song/api/com/br/song/domain/entity/Users.java
@@ -0,0 +1,20 @@
+package song.api.com.br.song.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+}
